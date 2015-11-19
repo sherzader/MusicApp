e2f302ed77eac2aadfd85b742433796fd4a1eae9
sheri zada <sheri.h.zada@gmail.com>
2015-11-19 11:30:56 -0800

    user find_by_credentials(email, password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,13 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64(16)
   end
 
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
