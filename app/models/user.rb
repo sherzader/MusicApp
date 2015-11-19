d7a7ab0516c7ca30e205bbec0fdf25b6fb49f0ba
sheri zada <sheri.h.zada@gmail.com>
2015-11-19 11:27:46 -0800

    user model takes password for bcrypt to hash and sets password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   attr_reader :password
 
+  validates :password, length: { minimum: 6, allow_nil: false }
   validates :password_digest, presence: true
   validates :email, :session_token, presence: true, uniqueness: true
   validate :generate_session_token, :ensure_session_token,
                                     :reset_session_token!
 
   def self.generate_session_token
-    self.session_token = SecureRandom.urlsafe_base64(16)
-
+    SecureRandom.urlsafe_base64(16)
   end
 
   def password=(password)
@@ -21,13 +21,13 @@ class User < ActiveRecord::Base
   end
 
   def reset_session_token!
-    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
   end
 
   private
   def ensure_session_token
-    self.session_token ||= SecureRandom.urlsafe_base64(16)
+    self.session_token ||= self.class.generate_session_token
   end
 end
